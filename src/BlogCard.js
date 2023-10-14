5261b47d7d8a8047df8f0dd6ef4ff67a63be0dd3
Yaswanthkumar1100 <yaswanthmmnnoo@gmail.com>
2023-10-14 14:27:58 +0530

    Simple

--- src/BlogCard.js
+++ src/BlogCard.js
@@ -4,7 +4,7 @@ import './BlogCard.css';
 const BlogCard = ({ title, content, image }) => {
   return (
     <div className="blog-card">
-      <img src={image} alt="Blog Image" className="blog-card-image" />
+      <img src={image} alt="Blog" className="blog-card-image" />
       <div className="blog-card-content">
         <h2 className="blog-card-title">{title}</h2>
         <p className="blog-card-text">{content}</p>
--- src/NavBarHead.js
+++ src/NavBarHead.js
@@ -1,9 +1,10 @@
-import React,{useState} from 'react';
-import { AppBar, Toolbar, Typography, Button, IconButton } from '@mui/material';
+import React, { useState } from 'react';
+import { AppBar, Toolbar, Typography, IconButton, Button } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import './NavBarHead.css';
 import Slider from './Slider';
 
+const PAGES = ["Home", "About", "Contact", "Blogs", "Initiatives"];
 
 export default function NavBarHead({ onNavigate, parallaxRef }) {
   const [drawerOpen, setDrawerOpen] = useState(false);
@@ -15,25 +16,20 @@ export default function NavBarHead({ onNavigate, parallaxRef }) {
   };
 
   return (
-    <AppBar className='appbar' style={{ backgroundColor: 'rgb(144, 203, 74)'}} >
+    <AppBar className='appbar' style={{ backgroundColor: 'rgb(144, 203, 74)'}}>
       <Toolbar>
-        <IconButton onClick={handleDrawerOpen}
-
-          edge="start"
-          color="inherit"
-          sx={{ marginRight: 2 }}
-        >
+        <IconButton onClick={handleDrawerOpen} edge="start" color="inherit" sx={{ marginRight: 2 }}>
           <MenuIcon />
         </IconButton>
-        <Slider open={drawerOpen} onClose={handleDrawerClose} onNavigate={onNavigate} parallaxRef={parallaxRef}/>
+        <Slider open={drawerOpen} onClose={handleDrawerClose} onNavigate={onNavigate} parallaxRef={parallaxRef} />
         <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
           Amalgam
         </Typography>
-        <Button color="inherit" onClick={() => onNavigate(0)}>Home</Button>
-        <Button color="inherit" onClick={() => onNavigate(1)}>About</Button>
-        <Button color="inherit" onClick={() => onNavigate(2)}>Contact</Button>
-        <Button color="inherit" onClick={() => onNavigate(3)}>Blogs</Button>
-        <Button color="inherit" onClick={() => onNavigate(4)}>Initiatives</Button>
+        {PAGES.map((page, index) => (
+          <Button key={index} color="inherit" onClick={() => onNavigate(index)}>
+            {page}
+          </Button>
+        ))}
       </Toolbar>
     </AppBar>
   );
